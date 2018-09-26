720db439811d6dd23963278970971a4345d70893
ns7767 <ns7767@IGM2550-17>
2018-09-25 21:50:25 -0400

    Initial Commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 cameraOffset;
+    public float smoothFactor;
+    void Start ()
+    {
+        cameraOffset = transform.position - playerTransform.position;
+	}	
+	void Update ()
+    {
+        Vector3 newPos = playerTransform.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+	}
+}
